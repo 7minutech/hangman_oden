5ee359b242e08e0b4b471c9902d21e69f2972793
7minutech <elijohnson1107@gmail.com>
2024-07-11 13:46:23 -0500

    Added pry-byebug fro debugging

--- lib/human.rb
+++ lib/human.rb
@@ -1,3 +1,4 @@
+require "pry-byebug"
 # human player
 class Human
   attr_accessor :guess
@@ -7,6 +8,11 @@ class Human
   end
 
   def guess
+    puts "Enter your guess: "
     self.guess = gets.chomp.downcase
   end
 end
+p1 = Human.new
+p1.guess
+binding.pry
+p1.guess
