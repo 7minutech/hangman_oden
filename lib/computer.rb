18daf2beaeb2f5e893d3332dfbc885bd2eb11ce8
7minutech <elijohnson1107@gmail.com>
2024-07-11 15:07:35 -0500

    Added set words method
    Method reads file and splits the words into arr
    which are filtered to word that are b/w 5 and 12

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,11 +1,18 @@
-require "google-10000-english-no-swears.txt"
 # computer player
 # will pick random word
 class Computer
-  attr_accessor :guess
+  attr_accessor :guess, :words
 
   def initialize
     @guess
     @words
   end
+
+  def set_words
+    words_file = File.read "google-10000-english-no-swears.txt"
+    words = words_file.split
+    words.filter! { |word| word.length.between?(5, 12) }
+  end
 end
+npc = Computer.new
+npc.set_words
