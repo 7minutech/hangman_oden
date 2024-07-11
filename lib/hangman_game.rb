1b77d85f8b65fd5e24ffb986cd73939734b2a80b
7minutech <elijohnson1107@gmail.com>
2024-07-11 15:31:37 -0500

    Added instance variables and attr accessor

--- lib/hangman_game.rb
+++ lib/hangman_game.rb
@@ -2,6 +2,11 @@ require "lib/computer"
 require "lib/human"
 # can play a game of hang man
 class HangmanGame
+  attr_accessor :player, :computer, :board
+
   def initialize
+    @player = Human.new
+    @computer = Computer.new
+    @board
   end
 end
