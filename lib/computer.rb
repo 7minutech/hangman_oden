5674a9dac5f5b0b535cc67b5352a2c86802c4bf3
7minutech <elijohnson1107@gmail.com>
2024-07-10 19:44:18 -0500

    Boilerplate

---
+++ lib/computer.rb
@@ -0,0 +1,6 @@
+# computer player
+# will pick random word
+class Computer
+  def initialize
+  end
+end
---
+++ lib/hangman_game.rb
@@ -0,0 +1,5 @@
+# can play a game of hang man
+class HangmanGame
+  def initialize
+  end
+end
