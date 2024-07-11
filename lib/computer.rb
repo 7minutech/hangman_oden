adddc76aef3ca5fe1a9a9c134d8e6929ba88844c
7minutech <elijohnson1107@gmail.com>
2024-07-11 15:10:34 -0500

    Added pick guess method
    Randomly picks a guess from words

--- lib/computer.rb
+++ lib/computer.rb
@@ -14,6 +14,11 @@ class Computer
     words = words_file.split
     words.filter! { |word| word.length.between?(5, 12) }
   end
+
+  def pick_guess
+    self.guess = words.sample
+  end
 end
 npc = Computer.new
 npc.set_words
+p npc.guess
