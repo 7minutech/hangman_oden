1244cadf26fd33898f0f5d44296a9c65787ce38d
7minutech <elijohnson1107@gmail.com>
2024-07-12 11:50:08 -0500

    Fixed typo in from json method

--- lib/hangman_game.rb
+++ lib/hangman_game.rb
@@ -134,7 +134,7 @@ class HangmanGame
   def self.from_json
     game_file_data = File.read("game_save.json")
     game_data = JSON.parse game_file_data
-    new(Human.new(game_data["player"]), Computer.new(game_data["computer"]), game_data[board],
+    new(Human.new(game_data["player"]), Computer.new(game_data["computer"]), game_data["board"],
         game_data["strikes"], game_data["game_over"], game_data["play_agin"])
   end
 
